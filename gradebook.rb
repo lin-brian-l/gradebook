0c101cb32ce1058207100466a8ebf0b07bef858f
Brian Lin <lin.brian.l@gmail.com>
2017-08-15 17:13:42 -0500

    Begin 8.5 - create gradebook

---
+++ gradebook.rb
@@ -0,0 +1,8 @@
+# Create a gradebook which allows the following:
+# - Add names to the gradebook
+# - Add assignments (and point values) to the gradebook
+# - Enter/edit point values
+# - Calculate grades
+
+require 'sqlite3'
+require 'faker' #for testing purposes
