c234ffda250bcd19364e80a447554659b8f80f0b
Brian Lin <lin.brian.l@gmail.com>
2017-08-15 17:21:27 -0500

    Initialize gradebook.db

--- gradebook.rb
+++ gradebook.rb
@@ -4,5 +4,21 @@
 # - Enter/edit point values
 # - Calculate grades
 
+#require gems
 require 'sqlite3'
 require 'faker' #for testing purposes
+
+#create SQLite3 database
+gb = SQLite3::Database.new("gradebook.db")
+gb.results_as_hash = true
+
+#create table variable
+create_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS gradebook(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255)
+  )
+SQL
+
+#create gradebook (if not made already)
+gb.execute(create_table)
