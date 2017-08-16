575d01fe79f9b90f3a65529951435c7b484d4f72
Brian Lin <lin.brian.l@gmail.com>
2017-08-16 17:09:14 -0500

    Finish many UI commands.

--- gradebook.rb
+++ gradebook.rb
@@ -1,11 +1,11 @@
 # Create a gradebook which allows the following:
-# - Create & test exporter method to trim extra key/value pairs
-# - Track for multiple courses : WIP
+# - Track for multiple courses : DONE
 # - Create UI : WIP
 # - Add names to the gradebook : DONE
 # - Add assignments (and point values) to the gradebook : DONE
 # - Enter/edit point values : DONE
 # - Calculate grades : DONE
+# - Create & test exporter method to trim extra key/value pairs : DONE
 
 
 #require gems
@@ -48,11 +48,10 @@ class Gradebook
 	  #enter "total" entry if doesn't exist
     empty_check = @gb.execute("SELECT name FROM #{@course}_total")
     @gb.execute(create_total_row) if empty_check.empty? || !empty_check[0].has_value?("total")
-
   end
 
   #clean export_all method
-  def export
+  def export_clean
     export_all = @gb.execute("SELECT * FROM #{@course}")
     export = []
     export_all.each do |student|
@@ -62,7 +61,7 @@ class Gradebook
   end
 
   #clean export_total method
-  def export_total
+  def export_total_clean
     export_total_raw = @gb.execute("SELECT * FROM #{@course}_total")
     export_total = export_total_raw[0]
     export_total.delete_if { |key, value| (0...export_total.keys.length).include?(key) }
@@ -74,9 +73,17 @@ class Gradebook
 	  gb.execute("INSERT INTO #{@course} (name) VALUES (?)", [name])
   end
 
+  #generate roster
+  def make_roster
+    roster = export_clean
+    roster.each { |student| puts "#{student.values[0]} | #{student.values[1]}" }
+  end
+
   #prompt for adding students
   def enter_name_ui
-    puts "Enter a student's name."
+    puts "Here is your current roster:"
+    make_roster
+    puts "Enter your new student's name."
     student = gets.chomp
     enter_name(@gb, student)
   end
@@ -84,15 +91,26 @@ class Gradebook
   #add assignments and total # of pts
   def enter_assignment(gb, name, total)
     gb.execute("ALTER TABLE #{@course} ADD COLUMN #{name} VARCHAR(255)")
+    gb.execute("UPDATE #{@course} SET #{name} = 0")
     gb.execute("ALTER TABLE #{@course}_total ADD COLUMN #{name} VARCHAR(255)")
     gb.execute("UPDATE #{@course}_total
       SET #{name} = '#{total}'
       ")
   end	
 
+  #create array of assignment names
+  def create_assignment_names
+    names_hash = export_total_clean
+    assignment_names = names_hash.keys
+    assignment_names.delete_at(0)
+    assignment_names
+  end
+
   #prompt for adding assignments and assigning totals
   def enter_assignment_ui
-    puts "Enter the name of the assignment."
+    puts "The following are all of the existing assignments:"
+    print create_assignment_names
+    puts "\nEnter the name of a new assignment."
     assignment = gets.chomp
     puts "Enter the number of points this assignment is worth."
     total = gets.chomp
@@ -106,20 +124,41 @@ class Gradebook
   	  WHERE name = '#{name}'")
   end
 
-  #prompt for enter score on assignment for individual student
+  #prompt for entering score on assignment for individual student
   def enter_score_ui
-    puts "Enter the name of the assignment."
-    assignment = gets.chomp
-    puts "Enter the name of the student."
+    puts "Here is the current gradebook:"
+    print_gradebook
+    puts "Enter the name of the student whose score you want to edit."
     student = gets.chomp
-    puts "Enter the student's score on the assignment."
+    puts "Enter the name of the assignment you want to edit scores for."
+    assignment = gets.chomp
+    puts "Enter the #{student}'s score on #{assignment}."
     score = gets.chomp
     enter_score(@gb, assignment, score, student)
+    puts "Here are #{student}'s updated grades:"
+    print_student_gradebook(student)
+  end  
+
+  #assignment commands UI
+  def assignment_ui
+    assignment_choice = nil
+    until (1..2).include?(assignment_choice)
+      puts "Would you like to (1) create a new assignment or (2) edit scores for an existing assignment?"
+      assignment_choice = gets.chomp.to_i
+      case assignment_choice
+      when 1
+        enter_assignment_ui
+      when 2
+        enter_score_ui
+      else
+        puts "Please enter the number 1 or 2."
+      end
+    end
   end
 
   #calculate total # of points
   def calc_total
-  	total = export_total
+  	total = export_total_clean
   	total_pts_array = total.values
   	index = 1
   	total_pts = 0
@@ -131,16 +170,14 @@ class Gradebook
   end
 
   #create student-assignment hash
-  def student_grades
-  	gradebook = @gb.execute("SELECT * FROM #{@course}")
-  	student_grades = {}
-  	gradebook.each do |student|
-  	  student_grades[student.values[1]] = {}
-      index = 2 
-      while index < (student.keys.length/2)
+  def student_grades_hash
+    gradebook = export_clean
+    student_grades = {}
+    gradebook.each do |student|
+      student_grades[student.values[1]] = {}
+      index = 2
+      while index < student.keys.length
         student_grades[student.values[1]].merge!(student.keys[index] => student.values[index])
-        # student_grades[student.values[1]][student.keys[index]] = student.values[index]
-        # puts student_grades
         index += 1
       end
   	end
@@ -158,7 +195,7 @@ class Gradebook
     student_total
   end
 
-  #calculate grades
+  #calculate student grades
   def calc_student_grade(student_total, total_pts)
   	student_percent = {}
   	student_grade = {}
@@ -175,7 +212,7 @@ class Gradebook
   	  67 => "D+",
   	  64 => "D", 
   	  60 => "D-", 
-  	  59 => "F"
+  	  0 => "F"
   	}
 
     student_total.each do |name, total|
@@ -186,7 +223,7 @@ class Gradebook
     student_percent.each do |name, percent|
       grades.each do |cutoff, grade|
       	if percent >= cutoff.to_f
-          student_grade[name] = [percent, grade]
+          student_grade[name] = [student_total[name], percent, grade]
           break if percent >= cutoff
       	end
       end
@@ -194,30 +231,76 @@ class Gradebook
     student_grade
   end
 
-  #print student grades
+  #print all student grades
   def print_grades
-  	all_grades = calc_student_grade(calc_student_total(student_grades), calc_total)
+  	all_grades = calc_student_grade(calc_student_total(student_grades_hash), calc_total)
     all_grades.each do |name, grade|
-      puts "#{name} scored #{grade[0]}, earning a #{grade[1]}."
+      puts "#{name} scored #{grade[1]}%, earning a #{grade[2]}."
     end
   end
 
-
   #print entire gradebook
   def print_gradebook
-    gradebook = @gb.execute("SELECT * FROM #{@course}")
+    total_hash = export_total_clean
+    gradebook = export_clean
     puts "Grades for #{@course}:"
     gradebook.each do |student|
       puts "- #{student['name']}:"
       index = 2 
-      while index < (student.keys.length/2)
-        puts "-- #{student.keys[index]} - #{student.values[index]}"
+      while index < (student.keys.length)
+        puts "-- #{student.keys[index]} - #{student.values[index]}/#{total_hash.values[index-1]}"
         index += 1
+      end
+      total = calc_total
+      student_total = calc_student_grade(calc_student_total(student_grades_hash), total)[student['name']]    
+      puts "-- Total : #{student_total[0]}/#{total}"
+      puts "-- Percent : #{student_total[1]}"
+      puts "-- Grade: #{student_total[2]}"
+    end
+  end
+
+  #print one student's grades
+  def print_student_gradebook(student)
+    total_hash = export_total_clean
+    gradebook = student_grades_hash
+    index = 1
+    puts "Grades for #{student} in #{@course}:"
+    while index < total_hash.values.length
+      puts "- #{gradebook[student].keys[index-1]} : #{gradebook[student].values[index-1]}/#{total_hash.values[index]}"
+      index += 1
+    end
+    student_total = calc_student_grade(calc_student_total(student_grades_hash), calc_total)[student]
+    total = calc_total
+    puts "- Total : #{student_total[0]}/#{total}"
+    puts "- Percent : #{student_total[1]}"
+    puts "- Grade: #{student_total[2]}"
+  end
+
+  #print commands UI
+  def print_ui
+    print_choice = nil
+    until (1..3).include?(print_choice) 
+      puts "Would you like to print (1) the entire gradebook, (2) all final grades, or (3) one student's grades?"
+      print_choice = gets.chomp.to_i
+      case print_choice
+      when 1
+        print_gradebook
+      when 2
+        print_grades
+      when 3
+        puts "Which student's grades would you like to see?"
+        student = gets.chomp
+        print_student_gradebook(student)
+      else
+        puts "Please enter a number from 1 - 3."
       end
     end
   end
 
 end
+
+# puts "What gradebook would you like to open?"
+# course = gets.chomp
 
 test = Gradebook.new("test")
 
@@ -226,5 +309,16 @@ test = Gradebook.new("test")
 # test.enter_score_ui
 # test.calc_total
 # test.print_gradebook
-# print test.export
-print test.calc_total
+# print test.export_total_clean
+# print test.print_grades
+# test.print_grades
+# test.enter_name_ui
+# print test.student_grades_hash
+# print test.calc_student_total(test.student_grades_hash)
+# print test.calc_student_grade(test.calc_student_total(test.student_grades_hash), test.calc_total)
+
+# test.print_ui
+
+# print test.create_assignment_names
+# test.print_student_gradebook("Brian")
+test.assignment_ui
